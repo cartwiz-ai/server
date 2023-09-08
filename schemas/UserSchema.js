cfda264b87bfcca7c44a84e431f0e5538449e382
tharunoptimus <tharunoptimus@outlook.com>
2023-09-09 00:17:37 +0530

    Changes to UserSchema

--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -5,8 +5,8 @@ const UserSchema = new Schema({
 	profilePic: { type: String, required: true },
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
+	username: { type: String, required: true },
 	age: { type: String, required: true },
-	phone: { type: String, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
 
