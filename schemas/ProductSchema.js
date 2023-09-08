303d9e00ec35eca1776dcda9191312b6051fd9d4
tharunoptimus <tharunoptimus@outlook.com>
2023-09-08 17:57:00 +0530

    Added ProductSchema

---
+++ schemas/ProductSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const ProductSchema = new Schema({
+	contentId: { type: String, required: true, unique: true },
+	gender: { type: String, required: true },
+	masterCategory: { type: String, required: true },
+	subCategory: { type: String, required: true },
+	articleType: { type: String, required: true },
+	baseColour: { type: String, required: true },
+	season: { type: String, required: true },
+	year: { type: String, required: true },
+	usage: { type: String, required: true },
+	productDisplayName: { type: String, required: true },
+	reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+},{ timestamps: true })
+
+let Product = mongoose.model("Product", ProductSchema)
+module.exports = Product
